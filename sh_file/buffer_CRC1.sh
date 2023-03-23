83cbaf47a758eadd6e85d5a577171d5827cd3af3
TongChen <89793506+CTDancer@users.noreply.github.com>
2023-03-23 16:02:14 +0800

    Update buffer_CRC1.sh
    deleted WANDB_KEY

--- sh_file/buffer_CRC1.sh
+++ sh_file/buffer_CRC1.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-export WANDB_API_KEY=b7b6ecceb6854bd12f58809f18264f979509d13b
 CUDA_VISIBLE_DEVICES=0,1,2,3 python buffer.py --dataset=CRC1 --model=ResNet18 --train_epochs=50 --num_experts=10 --lr_teacher=0.001 --batch_train=16 --batch_real=16 --decay --mom=0.9 --l2=0.01 --save_interval=10 --dsa=True --buffer_path=buffer_test --data_path={path_to_dataset}
