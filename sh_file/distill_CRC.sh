9bc58db6f912eb8e5a95c0a18783ccc7f9f5b52e
CTDancer <1163711472@qq.com>
2023-03-29 10:01:22 +0000

    add weight-decay and momentum hyper-parameters for distill

--- sh_file/distill_CRC.sh
+++ sh_file/distill_CRC.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
-CUDA_VISIBLE_DEVICES=7,6,5,4,3 python distill.py --dataset=CRC1 --model=ResNet18 --ipc=5 --pix_init=noise --num_eval=3 --epoch_eval_train=5000 --syn_steps=60 --batch_train=4 --batch_real=4 --expert_epochs=3 --max_start_epoch=5 --lr_init=1e-5 --lr_img=10000 --lr_lr=1e-09 --lr_teacher=0.00001 --buffer_path=buffer_long --data_path={path_to_dataset}
+export WANDB_API_KEY=b7b6ecceb6854bd12f58809f18264f979509d13b
+CUDA_VISIBLE_DEVICES=0,1,2,3,4 python distill.py --dataset=CRC1 --model=ResNet18 \
+ --ipc=5 \
+ --pix_init=noise \
+ --num_eval=3 \
+ --epoch_eval_train=1000 \
+ --syn_steps=50 \
+ --expert_epochs=5 \
+ --batch_train=4 \
+ --batch_real=4 \
+ --max_start_epoch=5 \
+ --lr_init=1e-5 \
+ --lr_img=10000 \
+ --lr_lr=1e-09 \
+ --lr_teacher=0.00001 \
+ --args.img_wd=1e-4 \
+ --args.lr_wd=1e-5 \
+ --args.img_mom=0.5 \
+ --args.lr_mom=0.5 \
+ --buffer_path=buffer_long --data_path={path_to_dataset}
