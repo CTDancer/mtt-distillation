7f77628dc22060bb61280c8c89f0b73177e48fd9
CTDancer <1163711472@qq.com>
2023-03-29 10:16:35 +0000

    update

--- sh_file/distill_CRC.sh
+++ sh_file/distill_CRC.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export WANDB_API_KEY=b7b6ecceb6854bd12f58809f18264f979509d13b
-CUDA_VISIBLE_DEVICES=0,1,2,3,4 python distill.py --dataset=CRC1 --model=ResNet18 \
+CUDA_VISIBLE_DEVICES=7,6,5,4,3 python distill.py --dataset=CRC1 --model=ResNet18 \
  --ipc=5 \
  --pix_init=noise \
  --num_eval=3 \
@@ -14,8 +14,8 @@ CUDA_VISIBLE_DEVICES=0,1,2,3,4 python distill.py --dataset=CRC1 --model=ResNet18
  --lr_img=10000 \
  --lr_lr=1e-09 \
  --lr_teacher=0.00001 \
- --args.img_wd=1e-4 \
- --args.lr_wd=1e-5 \
- --args.img_mom=0.5 \
- --args.lr_mom=0.5 \
+ --img_wd=1 \
+ --lr_wd=1e-11 \
+ --img_mom=0.5 \
+ --lr_mom=0.5 \
  --buffer_path=buffer_long --data_path={path_to_dataset}
